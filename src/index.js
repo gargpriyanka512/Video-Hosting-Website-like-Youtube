79cbe7f0eecf228ec43b973935756d6037398e91
gargpriyanka512 <gargpriyanka512@gmail.com>
2025-10-18 18:12:39 -0400

    connection to database is done

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
-import mongoose from "mongoose";
-import {DB_NAME} from "./constants";
-import express from "express";
-import connectDB from "./db";
-const app = express()
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path:"./env"
+})
 connectDB()
 
 
